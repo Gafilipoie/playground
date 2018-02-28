a911a566d5c8c606ceb5e82639851f1ee01b99e0
gafilipoie <marius.afilipoie@gmail.com>
2018-02-28 08:51:58 +0200

    working on encryption

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app'
+
+    constructor() {}
+
+    ngOnInit() {
+        this.encrypt('Hello World', 1);
+    }
+
+    encrypt(msg: string, s: number) {
+        let k: number[] = this.getCharCodes(msg);
+        let parity: number = 2 * s;
+        let n: number = k.length + parity;
+        console.log(`Encrypt Message '${msg}' with a length of ${k.length} where a parity length of ${parity} is added`);
+        console.log(`n = k + 2 * s => n = ${k.length} + 2 * ${s} => n = ${k.length + 2 * s}`);
+        // Each codeword contains 255 code word bytes, of which 223 bytes are data and 32 bytes are parity
+        let blockSize: number = 255 - parity;
+        let enc: number[] = [];
+
+        for (let i = 0; i < k.length; i += blockSize) {
+            let block = k.slice(i, i + blockSize);
+            //AM RAMAS AICI
+        }
+
+        return enc;
+    }
+
+    getCharCodes(msg: string) {
+        let charCodes: number[] = [];
+
+        for(let i = 0; i < msg.length; i++) {
+            charCodes.push(msg.charCodeAt(i));
+        }
+
+        return charCodes;
+    }
 }
