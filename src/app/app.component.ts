46b66ec3f5dd0de95575addfc3fb321935e7676d
Gabriel Afilipoie <marius.afilipoie@gmail.com>
2018-03-15 15:02:51 +0200

    refactored reed solomon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,31 +14,41 @@ import { ToBasePipe } from './shared/pipes/toBase.pipe';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    rs_m: number;
+    rs_s: number;
+    rs_p: number;
+    rs_m_to_encrypt: number[];
+    rs_encrypted: number[];
+    rs_encrypted_with_error: number[];
+    rs_decrypted:number[];
 
     constructor(
         private encrypt: EncryptService,
         private decrypt: DecryptService,
         private toBase: ToBasePipe) {}
 
-    ngOnInit() {
-        this.reedSolomon();
+    ngOnInit() {}
+
+    onSubmit(ngForm) {
+        this.rs_m = ngForm.form.value.rs_message;
+        this.rs_s = ngForm.form.value.rs_s;
+        this.rs_p = ngForm.form.value.rs_base;
+        ngForm.form.reset();
+        this.reedSolomon(this.rs_m, this.rs_s, this.rs_p);
     }
 
-    reedSolomon() {
-        let message: number = 29;
-        let s: number = 1;
-        let p: number = 11;
+    reedSolomon(m: number, s: number, p: number) {
+        this.rs_m_to_encrypt = this.toBase.transform(m, p);
+        console.log(`m(${this.rs_m_to_encrypt}) in base p(11): [${this.rs_m_to_encrypt.toString()}]`);
 
-        let m: number[] = this.toBase.transform(message, p);
-        console.log(`m(29) in base p(11): [${m.toString()}]`);
+        this.rs_encrypted = this.encrypt.init(this.rs_m_to_encrypt, s, p);
+        console.log(`Encrypted Message: [${this.rs_encrypted.toString()}]`);
 
-        let encrypted: number[] = this.encrypt.init(m, s, p);
-        console.log(`Encrypted Message: [${encrypted.toString()}]`);
+        this.rs_encrypted_with_error = JSON.parse(JSON.stringify(this.rs_encrypted));
+        this.rs_encrypted_with_error[1] = 2;
+        console.log(`Message with error: [${this.rs_encrypted_with_error.toString()}]`);
 
-        encrypted[1] = 2;
-        console.log(`Message with error: [${encrypted.toString()}]`);
-
-        let decrypted: number[] = this.decrypt.init(encrypted, p);
-        console.log(`Decrypted Message: [${decrypted.toString()}]`);
+        this.rs_decrypted = this.decrypt.init(this.rs_encrypted_with_error, p);
+        console.log(`Decrypted Message: [${this.rs_decrypted.toString()}]`);
     }
 }
