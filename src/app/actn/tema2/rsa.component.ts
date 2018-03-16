8b2c5231da35c9a97105b69233ceacaaec3c6662
Gabriel Afilipoie <marius.afilipoie@gmail.com>
2018-03-16 13:57:06 +0200

    small changes

--- src/app/actn/tema2/rsa.component.ts
+++ src/app/actn/tema2/rsa.component.ts
@@ -6,8 +6,16 @@ import { NgForm } from '@angular/forms';
     styleUrls: [ './rsa.component.scss' ]
 })
 export class RsaComponent implements OnInit {
+    rsa_m: number;
+    rsa_p: number;
 
     constructor() {}
 
     ngOnInit() {}
+
+    onSubmit(ngForm: NgForm) {
+        this.rsa_m = ngForm.form.value.rsa_message;
+        this.rsa_p = ngForm.form.value.rsa_base;
+        ngForm.form.reset();
+    }
 }
